7fd27d61093937983274c0858c70dd77ee6cbb7d
Yvahra <79484088+Yvahra@users.noreply.github.com>
2024-09-05 09:07:55 +0200

    ttf files

---
+++ examples.py
@@ -0,0 +1,7 @@
+import myPyPlot as mpp
+
+
+def bars():
+    x = [4, 2, 3, 1, 6]
+    y = [12, 24, 23, 17, 19]
+    mpp.customBars(x,y)
--- myPyPlot.py
+++ myPyPlot.py
@@ -1,2 +1,15 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.font_manager as font_manager
+import os
 
+
+fontpath = os.path.join(os.path.dirname(__file__), 'fonts/')
+font_files = font_manager.findSystemFonts(fontpaths=[fontpath, ])
+font_list = font_manager.createFontList(font_files)
+font_manager.fontManager.ttflist.extend(font_list)
+mpl.rcParams['font.family'] = 'Ubuntu'
+
+def customBars(x, y):
+    plt.bar(x,y)
+    plt.show()
